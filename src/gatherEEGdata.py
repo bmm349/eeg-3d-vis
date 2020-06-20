4ceb2fc505c4d44f7236539a0b7cc20f8a8ede3e
Brendan McFadden <bmm349@drexel.edu>
2020-06-20 17:38:18 -0400

    more eeg pipeline setup

--- src/gatherEEGdata.py
+++ src/gatherEEGdata.py
@@ -6,7 +6,7 @@ import threading
 import zmq
 import eeg_payload_pb2 as Message
 from abc import ABC,abstractmethod
-
+import threading
 class DataHandler(ABC):
     @abstractmethod
     def __init__(self, sensor_type='eeg', port=1331):
@@ -26,10 +26,35 @@ class EEGHandler(DataHandler):
         self.sock.setsockopt(zmq.LINGER, 1000)
         self.sock.setsockopt(zmq.IMMEDIATE, 1)
         self.sock.bind(f"tcp://127.0.0.1:{port}")
-    def start(self):
+        self.current_lsl_streams = pylsl.resolve_byprop('type', 'EEG', timeout=5)
+        if self.type == 'eeg':
+            # Set our data type to our chunk
+            self.dtype = EEGDataChunk(4, 12, None)
+        else:
+            print("other types are not implemented...")
+    def start(self, from_file=False):
+        self.inlet = pylsl.StreamInlet(self.current_lsl_streams[0], max_chunklen=getattr(self.dtype, 'maxchunklen')
+        assert getattr(self.dtype, 'n_channels') == self.inlet.info().channel_count()
         print("Starting any threads necessary...")
+        if (from_file):
+            # do file reading and looping here.
+        else:
+            # start muselsl piepline here.
+            t = threading.currentThread()
+            while getattr(t, 'do_run', True):
+                data, ts = self.inlet
+
     def stop(self):
         print("Stopping any threads necessary...")
+class AbstractDataChunk(ABC):
+    @abstractmethod
+    def __init__(self, data, *kwargs):
+        raise Exception("Dont implement an abstract method")
+class EEGDataChunk(AbstractDataChunk):
+    def __init__(self, nchan, maxchunklen=12, data=None):
+        self.n_channels = nchan
+        self.maxchunklen = maxchunklen
+        self.data = data
 if __name__=="__main__":
     print("Starting")
     boiledEggHandler = EEGHandler(port=1331)
